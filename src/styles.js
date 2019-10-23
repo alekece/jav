86cea9c4e4e0153a19a262b02bc25c2f253c294f
Alexis Le Provost <alexis.le-provost@ledger.fr>
2019-10-23 17:45:16 +0200

    reworked styles.js

--- src/styles.js
+++ src/styles.js
@@ -2,8 +2,8 @@ const foregroundColor = '#ecf0f1'
 const backgroundColor = '#2c3e50'
 const inputBackgroundColor = '#34495e'
 
-function input() {
-    return {
+function input(options) {
+    options.style = {
         bg: inputBackgroundColor,
         focus: {
             bg: 'red'
@@ -12,23 +12,30 @@ function input() {
             bg: 'red'
         }
     }
+
+    return options
 }
 
-function label() {
-    return {
+function label(options) {
+    options.style = {
         bg: backgroundColor,
         fg: foregroundColor
     }
+
+    return options
 }
 
-function helper() {
-    return {
+function helper(options) {
+    options.style = {
         fg: foregroundColor,
         bg: backgroundColor,
         border: {
-            bg: backgroundColor
+            bg: backgroundColor,
+            type: 'line'
         }
     }
+
+    return options
 }
 
 module.exports = {
