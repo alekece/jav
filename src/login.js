a7490dbd08698256f52dfc056663c44cbb21e3e6
Alexis Le Provost <alexis.le-provost@ledger.fr>
2019-10-23 22:02:43 +0200

    added ledger logo in login view

--- src/login.js
+++ src/login.js
@@ -5,41 +5,49 @@ const blessed = require('blessed'),
     widget = require('./widget'),
     fs = require('fs')
 
+
 function login(screen) {
     const auth = fs.readFileSync('./auth', 'utf8').split('\n')
 
     const grid = new contrib.grid({ rows: 12, cols: 12, screen: screen })
-    const form = grid.set(0, 0, 12, 12, blessed.form, {
+    const form = grid.set(0, 0, 10, 12, blessed.form, styles.form({
         keys: true,
         vi: false,
         parent: screen,
-        content: 'Authenticate'
+        label: ' Authenticate '
+    }))
+    const image = contrib.picture({
+        parent: form,
+        left: 50,
+        file: './assets/ledger.png',
+        cols: 110,
+        onReady: () => screen.render()
     })
     const emailLabel = blessed.text({
         parent: form,
         left: 2,
-        top: 2,
+        top: 15,
         name: 'emailLabel',
         content: 'Email:'
     });
     const tokenLabel = blessed.text({
         parent: form,
         left: 2,
-        top: 5,
+        top: 18,
         name: 'tokenLabel',
         content: 'Token:'
     });
     const urlLabel = blessed.text({
         parent: form,
         left: 2,
-        top: 8,
+        top: 21,
         name: 'jiraUrlLabel',
         content: 'JIRA URL:'
     })
     const rememberMeLabel = blessed.text({
         parent: form,
         left: 2,
-        top: 11,
+        top: 24,
         name: 'rememberMe',
         content: 'Remember me:'
     })
@@ -47,8 +55,9 @@ function login(screen) {
         parent: form,
         inputOnFocus: true,
         left: 2,
-        top: 3,
+        top: 16,
         height: 1,
+        width: '97%',
         name: 'email',
         value: auth[0]
     }));
@@ -57,8 +66,9 @@ function login(screen) {
         secret: true,
         inputOnFocus: true,
         left: 2,
-        top: 6,
+        top: 19,
         height: 1,
+        width: '97%',
         name: 'token',
         value: auth[1],
     }));
@@ -66,7 +76,8 @@ function login(screen) {
         parent: form,
         inputOnFocus: true,
         left: 2,
-        top: 9,
+        top: 22,
+        width: '97%',
         height: 1,
         name: 'jiraUrl',
         value: auth[2],
@@ -75,8 +86,9 @@ function login(screen) {
         parent: form,
         inputOnFocus: true,
         left: 2,
-        top: 12,
+        top: 25,
         height: 1,
+        width: '97%',
         name: 'rememberMe'
     })
 
@@ -94,7 +106,7 @@ function login(screen) {
 
     var helper = grid.set(10, 0, 2, 12, widget.helper, {
         shortcuts: [
-            { key: 'C-l', desc: 'Login', callback: () => form.submit() }
+            { key: ['C-l', 'l'], desc: 'Login', callback: () => form.submit() }
         ],
         shortcutByColumn: 4
     })
