626936da57d91b9334ab60f7570015dbd7627c55
Alexis Le Provost <alexis.le-provost@ledger.fr>
2019-10-23 19:16:47 +0200

    removed that f**king clear function

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ var blessed = require('blessed')
     , screen = blessed.screen()
     , { login } = require('./login')
 
-screen.clear = function() {
-    screen.children.forEach(element => screen.remove(element))
-}
-
 login(screen)
 
 screen.key(['escape', 'q', 'C-c'], function() {
