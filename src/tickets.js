efad13f2b9824cb84bbce60a6dd3626b4430f490
Alexis Le Provost <alexis.le-provost@ledger.fr>
2019-10-24 12:16:18 +0200

    fix var names

--- src/tickets.js
+++ src/tickets.js
@@ -20,7 +20,7 @@ grey
 const header = ['Issue', 'Type', 'Creator', 'Creation Date', 'Project','Status', 'Component', 'Summary']
 const keyBindings = [['i', 'key'], ['t', 'type'], ['c', 'creator'],
 ['d', 'created'], ['p', 'project'], ['s', 'status'], ['o', 'component'], ['s', 'summary']]
-const colors = ['magenta', 'green', 'cyan', 'grey', 'red', 'blue']
+const colorValues = ['magenta', 'green', 'cyan', 'grey', 'red', 'blue']
 
 async function fetchJiraTickets(jira, jql) {
     const issues = await jira.searchJira(jql);
